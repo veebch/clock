c58a5562919c8cf8e01dce8ef8a3eb7bdb155439
VEEB Projects <75116070+veebch@users.noreply.github.com>
2023-07-18 19:46:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# DCF receiver module https://de.elv.com/dcf-empfangsmodul-dcf-2-091610
-# DCF1 module receives DCF77 signal, see https://en.wikipedia.org/wiki/DCF77
+"""
+    Code to drive an emulator for a MutterUhr
+     DCF receiver module https://de.elv.com/dcf-empfangsmodul-dcf-2-091610
+     DCF1 module receives DCF77 signal, see https://en.wikipedia.org/wiki/DCF77
+    
+     Copyright (C) 2023 Veeb Projects https://veeb.ch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
 
 from machine import Pin, I2C
 from time import sleep, sleep_ms, ticks_ms, ticks_add, ticks_diff, mktime, localtime
